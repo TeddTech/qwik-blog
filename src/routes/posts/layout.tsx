d198371c21850e6164c46750730001bf5048c06b
TeddTech <ted_thompson@live.com>
2024-01-31 01:48:52 -0800

    protect post route

--- src/routes/posts/layout.tsx
+++ src/routes/posts/layout.tsx
@@ -1,17 +1,17 @@
 import { Slot, component$ } from "@builder.io/qwik";
-// import type { RequestHandler } from "@builder.io/qwik-city";
+import type { RequestHandler } from "@builder.io/qwik-city";
 
-// import type { Session } from "@auth/core/types";
+import type { Session } from "@auth/core/types";
 
-// export const onRequest: RequestHandler = (event) => {
-// 	const session: Session | null = event.sharedMap.get("session");
-// 	if (!session || new Date(session.expires) < new Date()) {
-// 		throw event.redirect(
-// 			302,
-// 			`/api/auth/signin?callbackUrl=${event.url.pathname}`,
-// 		);
-// 	}
-// };
+export const onRequest: RequestHandler = (event) => {
+	const session: Session | null = event.sharedMap.get("session");
+	if (!session || new Date(session.expires) < new Date()) {
+		throw event.redirect(
+			302,
+			`/api/auth/signin?callbackUrl=${event.url.pathname}`,
+		);
+	}
+};
 
 export default component$(() => {
 	return (
