a4337954001c3dd1237b2133716a1c94e2b772c5
Tedd Tech <ted_thompson@live.com>
2024-02-14 17:22:10 -0800

    Talking changlog (#13)
    * feat: ⚡️ new changlog talking
    
    * ci(release): 🔖 getting the changelog.md to gernerate

--- release.config.cjs
+++ release.config.cjs
@@ -1,13 +1,21 @@
 const config = {
+	name: "some-name",
+	description: "some description",
+	tagFormat: "v${version}alpha",
 	branches: ["main"],
 	plugins: [
 		"@semantic-release/commit-analyzer",
 		"@semantic-release/release-notes-generator",
-		"@semantic-release/changelog",
+		[
+			"@semantic-release/changelog",
+			{
+				changelogFile: "docs/CHANGELOG.md",
+			},
+		],
 		[
 			"@semantic-release/git",
 			{
-				assets: ["dist/*.js", "dist/*.js.map"],
+				assets: ["dist/*.js", "dist/*.js.map", "docs/CHANGELOG.md"],
 				message:
 					"chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
 			},
