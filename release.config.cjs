d594d2b0680e1461431df09d370453cd4d6d2c70
Tedd Tech <ted_thompson@live.com>
2024-02-14 19:09:13 -0800

    Markdownlint (#17)
    * fix: a
    
    * feat(docs): 📝 markdownlint rulee  m001

--- release.config.cjs
+++ release.config.cjs
@@ -17,7 +17,7 @@ const config = {
 			{
 				assets: ["dist/*.js", "dist/*.js.map", "docs/CHANGELOG.md"],
 				message:
-					"chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+					"chore(release): ${nextRelease.version}-alpha [skip ci]\n\n${nextRelease.notes}",
 			},
 		],
 		"@semantic-release/github",
