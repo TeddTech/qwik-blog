54accf8ed63cd8fa9409d2f6be26ee2e4ea6ae3f
TeddTech <ted_thompson@live.com>
2024-02-16 00:43:59 -0800

    fix: 🎨 added portected merge app

--- release.config.cjs
+++ release.config.cjs
@@ -2,7 +2,12 @@ const config = {
 	name: "some-name",
 	description: "some description",
 	tagFormat: "v${version}-alpha",
-	branches: ["main"],
+	branches: [
+		{
+			name: "beta",
+			prerelease: true,
+		},
+	],
 	plugins: [
 		"@semantic-release/commit-analyzer",
 		"@semantic-release/release-notes-generator",
@@ -17,7 +22,7 @@ const config = {
 			{
 				assets: ["dist/*.js", "dist/*.js.map", "docs/CHANGELOG.md"],
 				message:
-					"chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+					"chore(release): ${nextRelease.version}-alpha [skip ci]\n\n${nextRelease.notes}",
 			},
 		],
 		"@semantic-release/github",
