4f21d7e135b14ef979c0f4e2e263c1131ebc72da
Tedd Tech <ted_thompson@live.com>
2024-02-16 04:13:15 -0800

    Beta (#18)
    Got semantic release working
    
    ---------
    
    Co-authored-by: semantic-release-bot <semantic-release-bot@martynus.net>

--- release.config.cjs
+++ release.config.cjs
@@ -1,8 +1,17 @@
 const config = {
-	name: "some-name",
-	description: "some description",
-	tagFormat: "v${version}-alpha",
-	branches: ["main"],
+	branches: [
+		{
+			name: "main",
+		},
+		{
+			name: "beta",
+			prerelease: true,
+		},
+		{
+			name: "alpha",
+			prerelease: true,
+		},
+	],
 	plugins: [
 		"@semantic-release/commit-analyzer",
 		"@semantic-release/release-notes-generator",
@@ -10,12 +19,13 @@ const config = {
 			"@semantic-release/changelog",
 			{
 				changelogFile: "docs/CHANGELOG.md",
+				changelogTitle: "# Semantic Versioning Changelog",
 			},
 		],
 		[
 			"@semantic-release/git",
 			{
-				assets: ["dist/*.js", "dist/*.js.map", "docs/CHANGELOG.md"],
+				assets: ["dist/*.js", "dist/*.js.map", "docs/CHANGELOG.md", "package.json"],
 				message:
 					"chore(release): ${nextRelease.version}-alpha [skip ci]\n\n${nextRelease.notes}",
 			},
