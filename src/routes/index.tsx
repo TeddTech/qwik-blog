054ec2d9f57ba8ef2203f12347b4b64bd9a84886
TeddTech <ted_thompson@live.com>
2024-02-18 03:03:37 -0800

    fix: Pie commit.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -30,7 +30,7 @@ export default component$(() => {
 	return (
 		<>
 			<div class="mt-16 w-full max-w-5xl">
-				<div>Fried release</div>
+				<div>Pie release</div>
 				<Form>
 					<input
 						name="q"
