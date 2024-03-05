7da0290d9fb1eff2ca7a1e30ec6a64e12becd70e
TeddTech <ted_thompson@live.com>
2024-03-04 21:54:13 -0800

    feat(security): 🔒️ set content security policy

---
+++ src/routes/plugin@csp.ts
@@ -0,0 +1,24 @@
+import type { RequestHandler } from "@builder.io/qwik-city";
+import { isDev } from "@builder.io/qwik/build";
+
+export const onRequest: RequestHandler = (event) => {
+	if (isDev) return; // Will not return CSP headers in dev mode
+	const nonce = Date.now().toString(36); // Your custom nonce logic here
+	event.sharedMap.set("@nonce", nonce);
+	const csp = [
+		`default-src 'self'`,
+		`base-uri 'self'`,
+		`font-src 'self' https: data:`,
+		`form-action 'self'`,
+		`frame-ancestors 'self'`,
+		`img-src 'self' https://raw.githubusercontent.com/ data:`,
+		`object-src 'none'`,
+		`script-src 'self' https: 'nonce-${nonce}' 'strict-dynamic'`,
+		`frame-src 'self' 'nonce-${nonce}'`,
+		`script-src-attr 'none'`,
+		`style-src 'self' https: 'unsafe-inline'`,
+		"upgrade-insecure-requests",
+	];
+
+	event.headers.set("Content-Security-Policy", csp.join(";"));
+};
