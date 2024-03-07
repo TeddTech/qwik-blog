40cddaaca57da84ea015035d0d8d2d1160df7251
TeddTech <ted_thompson@live.com>
2024-03-06 22:39:38 -0800

    feat: add img csp

--- src/routes/plugin@csp.ts
+++ src/routes/plugin@csp.ts
@@ -1,17 +1,34 @@
 import type { RequestHandler } from "@builder.io/qwik-city";
 import { isDev } from "@builder.io/qwik/build";
 
-export const onRequest: RequestHandler = (event) => {
+export const onRequest: RequestHandler = async (event) => {
 	if (isDev) return; // Will not return CSP headers in dev mode
+
 	const nonce = Date.now().toString(36); // Your custom nonce logic here
 	event.sharedMap.set("@nonce", nonce);
+
+	const fetchProviders = await fetch(`${event.url.origin}/api/auth/providers`);
+	if (!fetchProviders.ok) {
+		throw new Error(
+			`Failed to fetch Provider Images: ${fetchProviders.statusText}`,
+		);
+	}
+
+	const providers = await fetchProviders.json();
+	const provider_images = Object.keys(providers)
+		.map(
+			(provider) =>
+				`https://authjs.dev/img/providers/${provider}.svg https://authjs.dev/img/providers/${provider}-dark.svg`,
+		)
+		.join(" ");
+
 	const csp = [
 		`default-src 'self'`,
 		`base-uri 'self'`,
 		`font-src 'self' https: data:`,
 		`form-action 'self'`,
 		`frame-ancestors 'self'`,
-		`img-src 'self' https://raw.githubusercontent.com/ data:`,
+		`img-src 'self' https://raw.githubusercontent.com/ ${provider_images} data:`,
 		`object-src 'none'`,
 		`script-src 'self' https: 'nonce-${nonce}' 'strict-dynamic'`,
 		`frame-src 'self' 'nonce-${nonce}'`,
@@ -20,5 +37,5 @@ export const onRequest: RequestHandler = (event) => {
 		"upgrade-insecure-requests",
 	];
 
-	event.headers.set("Content-Security-Policy", csp.join(";"));
+	event.headers.set("Content-Security-Policy", csp.join("; "));
 };
